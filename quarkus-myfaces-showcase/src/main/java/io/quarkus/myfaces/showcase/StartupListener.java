f1baa13bdf107835d269daa41f2b66758e2daa7a
rmpestano <rmpestano@gmail.com>
2019-06-07 19:23:23 -0300

    comment project stage

--- quarkus-myfaces-showcase/src/main/java/io/quarkus/myfaces/showcase/StartupListener.java
+++ quarkus-myfaces-showcase/src/main/java/io/quarkus/myfaces/showcase/StartupListener.java
@@ -3,13 +3,12 @@ package io.quarkus.myfaces.showcase;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @WebListener
 public class StartupListener implements ServletContextListener {
 
-    @ConfigProperty(name = "javax.faces.PROJECT_STAGE")
-    String projectStage;
+    /*@ConfigProperty(name = "javax.faces.PROJECT_STAGE")
+    String projectStage;*/
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
